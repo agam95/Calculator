14589ee9ee65d363aaaf7df416d74a833551f10e
Agam Singh <agam.s@hotmail.com>
2022-05-13 21:02:42 +0200

    JS start

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const setTheme = (theme) => (document.documentElement.className = theme);
+
+//BUTTONS
+const numbers = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
+const reset = document.getElementById("reset");
+const del = document.getElementById("del");
+const equal = document.getElementById("equal");
+
+//INPUT
+const input = document.getElementById("total");
