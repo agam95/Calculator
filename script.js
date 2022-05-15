a9f232919ef35e50d68d0c2ea7c2e2aaacbeceee
Agam Singh <agam.s@hotmail.com>
2022-05-15 16:42:22 +0200

    'Seperation

--- script.js
+++ script.js
@@ -5,7 +5,30 @@ const numbers = document.querySelectorAll(".number");
 const operator = document.querySelectorAll(".operator");
 const reset = document.getElementById("reset");
 const del = document.getElementById("del");
-const equal = document.getElementById("equal");
 
 //INPUT
 const input = document.getElementById("total");
+
+const buttons = [...numbers, ...operator, reset, del];
+
+buttons.forEach(function (i) {
+  i.addEventListener("click", function (event) {
+    // Access the clicked element
+    const target = event.target;
+
+    if (target.classList.contains("operator")) {
+      console.log("operator", target.value);
+      return;
+    }
+    if (target.classList.contains("decimal")) {
+      console.log("decimal", target.value);
+      return;
+    }
+    if (target.classList.contains("reset")) {
+      console.log("reset", target.value);
+      return;
+    }
+
+    console.log("number", target.value);
+  });
+});
