79bafa604c3afe0f1bedf18482eb7e9d038c92f3
Agam Singh <agam.s@hotmail.com>
2022-06-30 17:55:31 +0200

    Append numbers

--- script.js
+++ script.js
@@ -5,30 +5,50 @@ const numbers = document.querySelectorAll(".number");
 const operator = document.querySelectorAll(".operator");
 const reset = document.getElementById("reset");
 const del = document.getElementById("del");
+const decimal = document.querySelector(".decimal");
 
-//INPUT
+const buttons = [...numbers, ...operator, reset, del, decimal];
+
 const input = document.getElementById("total");
 
-const buttons = [...numbers, ...operator, reset, del];
-
-buttons.forEach(function (i) {
-  i.addEventListener("click", function (event) {
-    // Access the clicked element
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (event) {
     const target = event.target;
 
     if (target.classList.contains("operator")) {
+      input.value = target.value;
       console.log("operator", target.value);
       return;
     }
+
     if (target.classList.contains("decimal")) {
+      input.value = target.value;
       console.log("decimal", target.value);
       return;
     }
-    if (target.classList.contains("reset")) {
-      console.log("reset", target.value);
+
+    if (target.classList.contains("all-clear")) {
+      input.value = target.value;
+      console.log("clear", target.value);
       return;
     }
 
-    console.log("number", target.value);
+    console.log("digit", target.value);
+
+    if (target.matches("button")) {
+      const keyContent = target.value;
+      const displayedNum = input.value;
+      if (target.value) {
+        if (displayedNum === "0") {
+          input.value = keyContent;
+        } else {
+          input.value = displayedNum + keyContent;
+        }
+      }
+    }
   });
+});
+
+reset.addEventListener("click", function () {
+  input.value = 0;
 });
